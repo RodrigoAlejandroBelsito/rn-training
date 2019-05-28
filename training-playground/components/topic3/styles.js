e1da11bf99eef8bccc5c2bba8902b524feb2caeb
Rodrigo <rbelsito2@gmail.com>
2019-05-28 17:06:09 -0300

    Topic 3 - ii

--- training-playground/components/topic3/styles.js
+++ training-playground/components/topic3/styles.js
@@ -4,7 +4,7 @@ export default StyleSheet.create(
     {
         container: {
             flex: 1,
-            flexDirection: 'row',
+            flexDirection: 'column',
             alignContent: 'center',
             justifyContent: 'space-between',
             padding: 20,
