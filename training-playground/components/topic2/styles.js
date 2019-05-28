9ed6511b1ce255892a4185e40ed0ccff19991ba5
Rodrigo <rbelsito2@gmail.com>
2019-05-28 17:03:55 -0300

    Topic 3 - i

---
+++ training-playground/components/topic2/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create(
+    {
+        container: {
+            flex: 0.7,
+            width: 500,
+            height: 500,
+            justifyContent: 'space-between',
+            padding: 10,
+        },
+        box: {
+            borderRadius: 25,
+        },
+        box1: {
+            backgroundColor: '#00BFFF',
+            width: 100,
+            height: 100,
+        },
+        box2: {
+            backgroundColor: '#1E90FF',
+            width: 150,
+            height: 150,
+        },
+        box3: {
+            backgroundColor: '#6495ED',
+            width: 200,
+            height: 200,
+        }
+    }
+);
---
+++ training-playground/components/topic3/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create(
+    {
+        container: {
+            flex: 1,
+            flexDirection: 'row',
+            alignContent: 'center',
+            justifyContent: 'space-between',
+            padding: 20,
+        },
+        box: {
+            borderRadius: 25,
+        },
+        box1: {
+            backgroundColor: '#00BFFF',
+            width: 100,
+            height: 100,
+        },
+        box2: {
+            backgroundColor: '#1E90FF',
+            width: 100,
+            height: 100,
+        },
+        box3: {
+            backgroundColor: '#6495ED',
+            width: 100,
+            height: 100,
+        }
+    }
+);
